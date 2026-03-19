5c9d6e655e5faf27eb25bf04bf45a03b2ca1793f
Jose Joya <ing.jose.joya@gmail.com>
2026-03-19 18:06:16 -0500

    fix: vercel ssr deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+
+const IS_DEV = process.env.NODE_ENV === "development";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "server",
+  adapter: IS_DEV ? node({ mode: "standalone" }) : vercel(),
   server: {
     port: 3000,
     host: true,
